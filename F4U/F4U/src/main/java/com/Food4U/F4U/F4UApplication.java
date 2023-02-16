997dd0392ed0ea4abfab1aefaaedb1a9fcbbc409
Sanjay Patel <210122134@aston.ac.uk>
2023-02-16 14:51:12 +0000

    Added an example file for the project

---
+++ F4U/F4U/src/main/java/com/Food4U/F4U/F4UApplication.java
@@ -0,0 +1,13 @@
+package com.Food4U.F4U;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F4UApplication.class, args);
+	}
+
+}
