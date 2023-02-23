f072ab6dbca8536bc901de0c59ac74280b9e043d
Raikyu <57593049+Raikyudev@users.noreply.github.com>
2023-02-23 15:11:14 +0000

    Refactored the controller

--- src/main/java/com/food4u/website/controller/MainController.java
+++ src/main/java/com/food4u/website/controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class MainController {
     @GetMapping(value = "/login")
     public String loginPage() {
         return "login";
