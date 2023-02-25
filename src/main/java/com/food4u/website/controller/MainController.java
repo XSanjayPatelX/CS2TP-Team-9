971fed8e6504c344c9b31c3961967d0f1ed46f25
Sanjay Patel <Sanjay-_-Patel@outlook.com>
2023-02-25 12:19:47 +0000

    Fixed error with Mapping for login.jsp and MainController - See Description
    After doing a small bit of testing in the morning I managed to run into an error which cause the project to not start at all, after looking into the error I noticed that the error was cause by the mapping parameters which had the values "/login".
    
    This should now be working without an error and allowing users to login with the current static credentials - Email: Admin@f4u.com & Password: Root.

--- src/main/java/com/food4u/website/controller/MainController.java
+++ src/main/java/com/food4u/website/controller/MainController.java
@@ -4,16 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @GetMapping(value = "/login")
-    public String loginPage() {
-        return "login";
-    }
-
     @PostMapping(value = "/login")
     public String welcomePage(ModelMap modelMap, @RequestParam String email, @RequestParam String password) {
         if (email.equals("Admin@f4u.com") && password.equals("Root")) {
