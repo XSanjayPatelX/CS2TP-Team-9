ab1f7904e60de82daa12071e6aedba0a2a47fc69
NickK1506 <116087806+NickK1506@users.noreply.github.com>
2023-02-27 02:39:35 +0000

    Update Food_ItemRepository.java

--- src/main/java/com/food4u/website/repository/Food_ItemRepository.java
+++ src/main/java/com/food4u/website/repository/Food_ItemRepository.java
@@ -1,4 +1,5 @@
-package com.example.demo.Food_Item;
+package com.food4u.website.Food_ItemRepository;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
