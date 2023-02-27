c324f7f09b7fd570e770c6edbd2843c68823ad50
Hamzah <hcmzah@gmail.com>
2023-02-27 23:11:53 +0000

    UserController, clean up + naming conventions
    Co-authored-by: XSanjayPatelX <49167806+XSanjayPatelX@users.noreply.github.com>

--- src/main/java/com/food4u/website/controller/FoodItemController.java
+++ src/main/java/com/food4u/website/controller/FoodItemController.java
@@ -1,37 +1,37 @@
 package com.food4u.website.controller;
-import java.time.LocalDate;
-import com.food4u.website.entity.Food_Item;
-import com.food4u.website.repository.Food_ItemRepository;
-import jakarta.servlet.http.HttpSession;
+
+import com.food4u.website.entity.FoodItem;
+import com.food4u.website.repository.FoodItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import java.lang.*;
+
+import jakarta.servlet.http.HttpSession;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.lang.*;
+import java.time.LocalDate;
 
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class Food_ItemController {
-
-@Autowired
-private Food_ItemRepository repository;
+public class FoodItemController {
+    @Autowired
+    private FoodItemRepository repository;
 
     @GetMapping("/view")
     public ModelAndView getProducts(HttpSession session)
     {
         Map<String, Object> model = new HashMap<String, Object>();
-        List<Food_Item> myList = repository.findAll();
+        List<FoodItem> myList = repository.findAll();
         model.put("products", myList );
-        return new ModelAndView("editproduct", model);
+        return new ModelAndView("edit-product", model);
     }
 
-
     @PostMapping("/save")
-    public String save(@ModelAttribute Food_Item foodItem){
+    public String save(@ModelAttribute FoodItem foodItem){
         foodItem.setUpdatedAt(LocalDate.now());
         repository.save(foodItem);
         return "redirect:/view";
@@ -39,19 +39,18 @@ private Food_ItemRepository repository;
 
     @GetMapping("/showUpdateForm")
     public ModelAndView showUpdateForm(@RequestParam Integer id) {
-        ModelAndView mav = new ModelAndView("updateproduct");
-        Food_Item foodItem = repository.findById(id).get();
-        String title = "Edit an existing product";
-        mav.addObject("title",title);
+        ModelAndView mav = new ModelAndView("update-product");
+        FoodItem foodItem = repository.findById(id).get();
+
+        mav.addObject("title", "Edit an existing product");
         mav.addObject("products", foodItem);
         return mav;
     }
+
     @GetMapping("/newProduct")
     public ModelAndView showUpdateForm(){
-        ModelAndView mav = new ModelAndView("updateproduct");
-        String title = "Add a new product";
-        mav.addObject("title",title);
+        ModelAndView mav = new ModelAndView("update-product");
+        mav.addObject("title", "Add a new product");
         return mav;
     }
-
 }
--- src/main/java/com/food4u/website/controller/MainController.java
+++ src/main/java/com/food4u/website/controller/MainController.java
@@ -5,11 +5,7 @@ import com.food4u.website.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Calendar;
 
 @Controller
 public class MainController {
@@ -26,16 +22,6 @@ public class MainController {
         return "product";
     }
 
-    @GetMapping(value = "editproduct")
-    public String editproductPage() {
-        return "editproduct";
-    }
-
-    @GetMapping(value = "updateproduct")
-    public String updateproductPage() {
-        return "updateproduct";
-    }
-
     @GetMapping(value = "/aboutus")
     public String aboutusPage() {
         return "aboutus";
@@ -46,36 +32,9 @@ public class MainController {
         return "login";
     }
 
-    @PostMapping(value = "/login")
-    public String loginSubmit(User user, ModelMap modelMap) {
-
-        if (userRepo.existsByEmail(user.getEmail())) {
-            User u = userRepo.findByEmail(user.getEmail());
-
-            if (u.getPassword().equals(user.getPassword())) {
-                modelMap.put("user", u);
-                return "welcome";
-            }
-        }
-
-        modelMap.put("errorMsg", "Email or password is incorrect");
-        return "login";
-    }
-
     @GetMapping(value = "/register")
     public String registerPage(Model model) {
         model.addAttribute("user", new User());
         return "register";
-    }
-
-    @PostMapping(value = "/register")
-    public String registerSubmit(User user) {
-        user.setPassword(user.getPassword()); // TODO: Encrypt password
-        user.setIsAdmin(false);
-        user.setJoinDate(new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
-
-        userRepo.save(user);
-
-        return "welcome";
     }
 }
