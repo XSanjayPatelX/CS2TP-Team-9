82564484338c23264e5ec105fb7c09dd1db42493
NickK1506 <116087806+NickK1506@users.noreply.github.com>
2023-02-27 02:37:27 +0000

    Delete src/main/java/com/example/demo directory

--- src/main/java/com/example/demo/Food_Item/Food_ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Food_Item;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Food_ItemRepository extends JpaRepository<Food_Item, Integer> {
-}
