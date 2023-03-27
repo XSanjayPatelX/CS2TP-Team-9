1f53fee1fd9a9e8a2b60c0e137de3750a24a574c
NickK1506 <210091988@aston.ac.uk>
2023-03-27 03:33:36 +0100

    added search bar and querying, fixed centering

--- src/main/java/com/food4u/website/repository/FoodRepository.java
+++ src/main/java/com/food4u/website/repository/FoodRepository.java
@@ -2,8 +2,17 @@ package com.food4u.website.repository;
 
 import com.food4u.website.entity.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FoodRepository extends JpaRepository<FoodItem, Integer> {
+
+    @Query(value="select * from food_item e where e.name like %:keyword% or e.category like %:keyword%", nativeQuery = true)
+    List<FoodItem> findByKeyword(@Param("keyword") String keyword);
+
+
 }
