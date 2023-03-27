b8372119ac509eb3b46df3c3ebe373326023bf2e
Raikyu <57593049+Raikyudev@users.noreply.github.com>
2023-03-27 17:30:41 +0100

    added users to the admin page

--- src/main/java/com/food4u/website/controller/AdminController.java
+++ src/main/java/com/food4u/website/controller/AdminController.java
@@ -1,15 +1,18 @@
 package com.food4u.website.controller;
 
 import com.food4u.website.entity.Orders;
+import com.food4u.website.entity.User;
 import com.food4u.website.repository.FoodRepository;
 import com.food4u.website.repository.OrderItemRepository;
 import com.food4u.website.repository.OrderRepository;
+import com.food4u.website.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,6 +31,9 @@ public class AdminController {
     @Autowired
     private FoodRepository foodRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/orders")
     public ModelAndView orders() {
         Map<String, Object> model = new HashMap<String, Object>();
@@ -43,5 +49,22 @@ public class AdminController {
         model.put("items", orderItemRepository.findAllByOrderId(id));
 
         return new ModelAndView("view-order", model);
+    }
+
+    @RequestMapping("/users")
+    public ModelAndView users() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        List<User> myList = userRepository.findAll();
+        model.put("users", myList);
+        return new ModelAndView("users", model);
+    }
+
+    @GetMapping("/users/{email}")
+    public ModelAndView viewUser(@PathVariable("email") String email) {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("user", userRepository.findByEmail(email));
+
+
+        return new ModelAndView("view-user", model);
     }
 }
